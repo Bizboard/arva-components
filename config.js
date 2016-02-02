a0fd8f2913d25bd7392bb0fc66281fd360b3a647
Tom Clement <mr@tomclement.nl>
2016-02-02 15:58:03 +0100

    Updated dependencies

--- config.js
+++ config.js
@@ -1,50 +1,71 @@
 System.config({
   baseURL: "/",
   defaultJSExtensions: true,
-  transpiler: "traceur",
+  transpiler: "babel",
+  babelOptions: {
+    "stage": 0,
+    "optional": [
+      "runtime",
+      "es7.decorators",
+      "es7.asyncFunctions",
+      "optimisation.modules.system"
+    ]
+  },
   paths: {
     "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
+    "npm:*": "jspm_packages/npm/*",
+    "bitbucket:*": "jspm_packages/bitbucket/*"
+  },
+
+  meta: {
+    "/*.css": {
+      "loader": "systemjs/plugin-css"
+    },
+    "/*.svg": {
+      "loader": "arva-utils/ImageLoader"
+    },
+    "/*.png": {
+      "loader": "arva-utils/ImageLoader"
+    },
+    "/*.jpg": {
+      "loader": "arva-utils/ImageLoader"
+    }
   },
 
   map: {
     "arva-ds": "github:bizboard/arva-ds@develop",
     "arva-utils": "github:bizboard/arva-utils@develop",
+    "babel": "npm:babel-core@5.8.35",
+    "babel-runtime": "npm:babel-runtime@5.8.35",
+    "core-js": "npm:core-js@1.2.6",
     "di.js": "github:bizboard/di.js@master",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.90",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
     "github:bizboard/SPSoapAdapter@develop": {
-      "arva-utils": "github:bizboard/arva-utils@master",
+      "arva-utils": "github:bizboard/arva-utils@develop",
       "eventemitter3": "npm:eventemitter3@1.1.1",
       "json-query": "npm:json-query@1.5.0",
-      "lodash": "npm:lodash@3.10.1",
-      "xml2js": "npm:xml2js@0.4.12"
+      "lodash": "npm:lodash@4.2.0",
+      "xml2js": "npm:xml2js@0.4.16"
     },
     "github:bizboard/arva-ds@develop": {
       "SPSoapAdapter": "github:bizboard/SPSoapAdapter@develop",
       "arva-utils": "github:bizboard/arva-utils@develop",
       "di": "github:bizboard/di.js@master",
       "eventemitter3": "npm:eventemitter3@1.1.1",
-      "firebase": "github:firebase/firebase-bower@2.3.1",
-      "lodash": "npm:lodash@3.10.1"
+      "firebase": "github:firebase/firebase-bower@2.4.0",
+      "lodash": "npm:lodash@4.2.0"
     },
     "github:bizboard/arva-utils@develop": {
+      "bowser": "npm:bowser@1.0.0",
       "di": "github:bizboard/di.js@master",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "lodash": "npm:lodash@3.10.1",
-      "path": "github:jspm/nodelibs-path@0.1.0"
-    },
-    "github:bizboard/arva-utils@master": {
-      "di": "github:bizboard/di.js@master",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "lodash": "npm:lodash@3.10.1",
+      "lodash": "npm:lodash@4.2.0",
       "path": "github:jspm/nodelibs-path@0.1.0"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.5.1"
+      "buffer": "npm:buffer@3.6.0"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -62,7 +83,7 @@ System.config({
       "string_decoder": "npm:string_decoder@0.10.31"
     },
     "github:jspm/nodelibs-timers@0.1.0": {
-      "timers-browserify": "npm:timers-browserify@1.4.1"
+      "timers-browserify": "npm:timers-browserify@1.4.2"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
@@ -70,12 +91,24 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:buffer@3.5.1": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "ieee754": "npm:ieee754@1.1.6",
-      "is-array": "npm:is-array@1.0.1"
+    "npm:babel-runtime@5.8.35": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:core-util-is@1.0.1": {
+    "npm:buffer@3.6.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "ieee754": "npm:ieee754@1.1.6",
+      "isarray": "npm:isarray@1.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:core-js@1.2.6": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-util-is@1.0.2": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
     "npm:inherits@2.0.1": {
@@ -84,7 +117,7 @@ System.config({
     "npm:json-query@1.5.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:lodash@3.10.1": {
+    "npm:lodash@4.2.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
@@ -95,7 +128,7 @@ System.config({
     },
     "npm:readable-stream@1.1.13": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "core-util-is": "npm:core-util-is@1.0.1",
+      "core-util-is": "npm:core-util-is@1.0.2",
       "events": "github:jspm/nodelibs-events@0.1.1",
       "inherits": "npm:inherits@2.0.1",
       "isarray": "npm:isarray@0.0.1",
@@ -103,7 +136,7 @@ System.config({
       "stream-browserify": "npm:stream-browserify@1.0.0",
       "string_decoder": "npm:string_decoder@0.10.31"
     },
-    "npm:sax@1.1.3": {
+    "npm:sax@1.1.5": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "stream": "github:jspm/nodelibs-stream@0.1.0",
@@ -117,24 +150,22 @@ System.config({
     "npm:string_decoder@0.10.31": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
-    "npm:timers-browserify@1.4.1": {
+    "npm:timers-browserify@1.4.2": {
       "process": "npm:process@0.11.2"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:xml2js@0.4.12": {
+    "npm:xml2js@0.4.16": {
       "events": "github:jspm/nodelibs-events@0.1.1",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.2",
-      "sax": "npm:sax@1.1.3",
+      "sax": "npm:sax@1.1.5",
       "timers": "github:jspm/nodelibs-timers@0.1.0",
-      "util": "github:jspm/nodelibs-util@0.1.0",
-      "xmlbuilder": "npm:xmlbuilder@3.1.0"
+      "xmlbuilder": "npm:xmlbuilder@4.2.1"
     },
-    "npm:xmlbuilder@3.1.0": {
-      "lodash": "npm:lodash@3.10.1",
+    "npm:xmlbuilder@4.2.1": {
+      "lodash": "npm:lodash@4.2.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
